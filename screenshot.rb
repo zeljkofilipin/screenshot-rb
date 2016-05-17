8692e238e495e1da68f185fb6b0e8620b3b95d0c
Željko Filipin <zeljko.filipin@gmail.com>
2016-05-17 18:24:31 +0200

    Take screen shot with screencapture if Selenium raises Selenium::WebDriver::Error::UnknownError
    https://github.com/zeljkofilipin/screenshot-js/issues/1

--- screenshot.rb
+++ screenshot.rb
@@ -1,5 +1,11 @@
 require 'selenium-webdriver'
 driver = Selenium::WebDriver.for :firefox
 driver.navigate.to ARGV[0]
-driver.save_screenshot('screenshot.png')
-driver.quit
+begin
+  driver.save_screenshot('screenshot.png')
+rescue Selenium::WebDriver::Error::UnknownError => e
+  puts "Uh oh. Selenium said: '#{e}'. Trying to take screenshots with screencapture. This will work only on Mac."
+  `screencapture screenshot.png`
+ensure
+  driver.quit
+end
